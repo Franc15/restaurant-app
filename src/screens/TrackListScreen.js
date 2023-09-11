d2dd2d8c360671db8ede5250a07e2f99ff82634c
sasatee <123723844+sasatee@users.noreply.github.com>
2023-09-11 11:36:49 +0400

    testt

---
+++ src/screens/TrackListScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <>
+      <SafeAreaView forceInset={{ top: "always" }}></SafeAreaView>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Button
+        title="Go to Track Detail"
+        onPress={() => navigation.navigate("TrackDetail")}
+      />
+      <SafeAreaView />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackListScreen;
