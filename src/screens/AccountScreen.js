13d9d4e900d953693645845519b2c03b137913d8
sasatee <123723844+sasatee@users.noreply.github.com>
2023-09-19 12:21:56 +0400

    edit account screen

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSelector } from 'react-redux';
 
-const AccountScreen = () => {
+const  AccountScreen = () => {
+  const userData = useSelector((state) => state.auth.user);
+  // const [userData, setUserData] = useState(null);
+
+  // useEffect(() => {
+  //   // Retrieve user data from AsyncStorage
+  //   const getUserData = async () => {
+  //     try {
+  //       const userDataString = await AsyncStorage.getItem('userData');
+  //       if (userDataString) {
+  //         const parsedUserData = JSON.parse(userDataString);
+  //         setUserData(parsedUserData);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error retrieving user data:', error);
+  //     }
+  //   };
+
+  //   getUserData();
+  // }, []);
+
   return (
-    <View>
-        <Text style={{ fontSize: 48 }}>Account Screen</Text>
-    </View>
-  )
+    <SafeAreaView>
+      {userData ? (
+        <View>
+          <Text>User ID: {userData.userId}</Text>
+          <Text>Email: {userData.email}</Text>
+          <Text>Name: {userData.name}</Text>
+          
+        </View>
+      ) : (
+        <Text>Loading user data...</Text>
+      )}
+    </SafeAreaView>
+  );
 }
 
-export default AccountScreen
+export default AccountScreen;
